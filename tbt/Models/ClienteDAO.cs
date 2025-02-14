00a7838e2c8815cba6de87d2f2a4a86eb289556a
VITORIA3A <134731904+VitoriaIFRO2A@users.noreply.github.com>
2025-02-14 15:10:54 -0400

    commit atualizado

---
+++ tbt/Models/ClienteDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using tbt.database;
+
+
+namespace tbt.Models
+{
+    internal class ClienteDAO
+    {
+        private static Conexao _conn = new Conexao();
+        public void Insert(Cliente obj_cli)
+        {
+            try
+            {
+                var comando = _conn.Query();
+
+                comando.CommandText = "INSERT INTO cliente VALUES (null, @nome, @cpf, @telefone, @estado, @cidade, @estado, @cidade, @rua, @numero, @bairro);" +
+
+                comando.Parameters.AddWithValue("@nome", obj_cli.nome);
+                comando.Parameters.AddWithValue("@cpf", obj_cli.cpf);
+                comando.Parameters.AddWithValue("@telefone", obj_cli.telefone);
+                comando.Parameters.AddWithValue("@estado", obj_cli.estado);
+                comando.Parameters.AddWithValue("@cidade", obj_cli.cidade);
+                comando.Parameters.AddWithValue("@rua", obj_cli.rua);
+                comando.Parameters.AddWithValue("@numero", obj_cli.numero);
+                comando.Parameters.AddWithValue("@bairro", obj_cli.bairro);
+
+
+                var resultado = comando.ExecuteNonQuery();
+
+                if (resultado == 0)
+                {
+                    throw new Exception("Ocorreram erros ao salvar as informações");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- tbt/Models/Funcionario.cs
+++ tbt/Models/Funcionario.cs
@@ -1,34 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace tbt.Models
 {
-    internal class Cliente
+    internal class Funcionario
     {
         public string nome { get; set; }
+        public DateTime? data_nascimento { get; set; }
         public string cpf { get; set; }
+        public string rg { get; set; }
+        public string sexo { get; set; }
+        public string funcao { get; set; }
         public string telefone { get; set; }
+        public string email { get; set; }
         public string estado { get; set; }
         public string cidade { get; set; }
         public string rua { get; set; }
         public int numero { get; set; }
         public string bairro { get; set; }
 
-
-        public Cliente() { }
-        public Cliente(string nome, string cpf, string telefone, string estado, string cidade, string rua, int numero, string bairro)
+        public Funcionario(string nome, DateTime data_nascimento, string cpf, string rg, string sexo, string funcao, string telefone, string email, string estado, string cidade, int numero, string bairro)
         {
             this.nome = nome;
+            this.data_nascimento = data_nascimento;
             this.cpf = cpf;
+            this.rg = rg;
+            this.sexo = sexo;
+            this.funcao = funcao;
             this.telefone = telefone;
+            this.email = email;
             this.estado = estado;
             this.cidade = cidade;
-            this.rua = rua;
             this.numero = numero;
             this.bairro = bairro;
         }
+        public Funcionario() { }
     }
 }

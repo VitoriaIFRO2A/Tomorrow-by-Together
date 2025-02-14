78c8d477645170b824d5e85ad86fb90b5bb45fee
VITORIA3A <134731904+VitoriaIFRO2A@users.noreply.github.com>
2025-02-14 14:59:42 -0400

    commit atualizacao designer

--- tbt/Telas/ConsultarFornecedor.xaml.cs
+++ tbt/Telas/ConsultarFornecedor.xaml.cs
@@ -23,5 +23,10 @@ namespace tbt.Telas
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
